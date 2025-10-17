1ed45e6cf5c20bb9287e63d57612d7453aa96870
Shaq1030 <sikder.ishaq62@student.qcc.cuny.edu>
2025-10-17 23:45:10 +0000

    Task 1 complete

---
+++ HW2/Square.h
@@ -0,0 +1,9 @@
+// Sikder Task 1
+#ifndef SQUARE_H
+#define SQUARE_H
+
+class Square {
+//class
+};
+
+#endif // Square.h
---
+++ HW2/driver.cpp
@@ -0,0 +1,11 @@
+// driver.cpp
+// Sikder Task 1
+// 10/17/25
+// creating empty main function
+
+#include <iostream>
+#include "Square.h"
+
+int main() {
+    return 0;
+}
